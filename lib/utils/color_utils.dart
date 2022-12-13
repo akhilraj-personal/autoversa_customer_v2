45fef423d5da7163fb9f85e84e76f6450c6105b2
unknown <rbharadkrishna@gmail.com>
2022-12-13 14:32:22 +0530

    signup new screen

--- lib/utils/color_utils.dart
+++ lib/utils/color_utils.dart
@@ -9,5 +9,6 @@ Color greyColor = const Color(0xff999999);
 Color lightblackColor = const Color(0xff333333);
 Color borderGreyColor = const Color(0xffCCCCCC);
 Color changenumberorange = const Color(0xffFF6833);
+Color warningcolor = const Color(0xfff0ad4e);
 Color lightGreyColor = const Color(0xffEFEFEF);
 Color container_grey_color = const Color(0xffF1F1F1);
