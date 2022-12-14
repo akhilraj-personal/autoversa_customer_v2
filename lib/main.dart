da723625b0f452dd4c177465747766b65b38f6c8
Akhil Raj <akhil321.ak@gmail.com>
2022-12-14 13:01:17 +0400

    otp screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'dart:io' show Platform;
+
 import 'package:autoversa/provider/provider.dart';
 import 'package:autoversa/screens/auth_screens/login_page.dart';
-import 'package:autoversa/screens/auth_screens/signup_page.dart';
 import 'package:autoversa/screens/bottom_tab/bottomtab.dart';
 import 'package:autoversa/screens/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:provider/provider.dart';
 
 import 'generated/l10n.dart';
@@ -14,6 +16,17 @@ import 'generated/l10n.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  if (Platform.isIOS || Platform.isAndroid) {
+    try {
+      await OneSignal.shared.setAppId("71139c93-b725-4209-9a36-619ce631f943");
+      OneSignal.shared.setNotificationWillShowInForegroundHandler(
+          (OSNotificationReceivedEvent? event) {
+        return event?.complete(event.notification);
+      });
+    } catch (e) {
+      print('${e.toString()}');
+    }
+  }
   SystemChrome.setPreferredOrientations(<DeviceOrientation>[
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown
@@ -64,7 +77,6 @@ var routes = <String, WidgetBuilder>{
         index: 1,
       ),
   Routes.loginPage: (BuildContext context) => LoginPage(),
-  Routes.signup: (BuildContext context) => SignupPage(),
 };
 
 class Routes {
@@ -73,5 +85,4 @@ class Routes {
   static const loginPage = "screens/auth_screens/login_page.dart";
   static const otpverification =
       "screens/auth_screens/verification_by_otp_page.dart";
-  static const signup = "screens/auth_screens/signup_page.dart";
 }
